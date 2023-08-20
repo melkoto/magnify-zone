7d6d6e513da2efe5d329ebd695dabf3413ab63dc
Movsar Makhmutov <m.movsar@gmail.com>
2023-08-20 15:26:48 +0300

    main

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import peerDeps from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+
+import { terser } from 'rollup-plugin-terser'
+
+export default [
+    {
+        input: 'src/index.js',
+        output: [
+            {
+                file: 'dist/index.js',
+                format: 'cjs',
+            },
+            {
+                file: 'dist/index.es.js',
+                format: 'es',
+                exports: 'named',
+            },
+        ],
+        plugins: [
+            babel({
+                exclude: 'node_modules/**',
+                presets: ['@babel/preset-react'],
+            }),
+            peerDeps(),
+            nodeResolve(),
+            postcss({
+                plugins: [],
+                minimize: true,
+            }),
+            terser(),
+        ],
+    },
+]
--- src/components/Magnify/Magnify.jsx
+++ src/components/Magnify/Magnify.jsx
@@ -1,5 +1,47 @@
-export const Magnify = () => (
-    <div>
-        <h1>Magnify</h1>
-    </div>
-)
+import { useState } from 'react'
+
+import './Magnify.css'
+
+export const Magnify = ({ imageUrl, zoomFactor }) => {
+    const [position, setPosition] = useState({ x: 0, y: 0 })
+    const [isVisible, setIsVisible] = useState(false)
+
+    const handleMouseMove = (event) => {
+        setPosition({ x: event.clientX, y: event.clientY })
+    }
+
+    const handleMouseEnter = () => {
+        setIsVisible(true)
+    }
+
+    const handleMouseLeave = () => {
+        setIsVisible(false)
+    }
+
+    const { x, y } = position
+
+    return (
+        <div className="magnify-container">
+            <div
+                className="magnify-image-container"
+                onMouseMove={handleMouseMove}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <img src={imageUrl} alt="Image" />
+                {isVisible && (
+                    <div
+                        className="magnify-zoom"
+                        style={{
+                            backgroundImage: `url(${imageUrl})`,
+                            backgroundPosition: `-${x * zoomFactor}px -${
+                                y * zoomFactor
+                            }px`,
+                            transform: `scale(${1 / zoomFactor})`,
+                        }}
+                    ></div>
+                )}
+            </div>
+        </div>
+    )
+}
